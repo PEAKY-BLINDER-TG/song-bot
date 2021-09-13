8fd437ad95a5dc6d45f473a86dc2d48bfda37b4e
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-13 11:39:15 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -29,7 +29,7 @@ async def start(client, message):
                     InlineKeyboardButton(BUTTON1, url='https://t.me/cinemazilla'),
                  ],[
                     InlineKeyboardButton(OWNER, url=f"https://telegram.dog/{Config.OWNER}"),
-                    InlineKeyboardButton(ABS, url=B2)
+                    InlineKeyboardButton(ABS, url='https://t.me/wasim')
             ]
           ]
         ),
